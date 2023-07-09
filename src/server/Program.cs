0596c42f1027e566d3c0a810aee9319813a990d7
bakery <39867159+BakeryD@users.noreply.github.com>
2023-07-09 18:33:00 -0500

    init from template

---
+++ src/server/Program.cs
@@ -0,0 +1,26 @@
+using Power2People.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddCors();
+
+var app = builder.Build();
+app.UseStaticFiles();
+app.MapFallbackToFile("index.html");
+
+var libPath = Path.Combine(app.Environment.WebRootPath, "Content");
+var contentLibrary = new ContentLibrary(libPath).Load();
+
+app.UseCors(builder => builder
+    .AllowAnyOrigin()
+    .AllowAnyMethod()
+    .AllowAnyHeader()
+);
+
+//load the routes
+Power2People.Api.Content.MapRoutes(app, contentLibrary);
+app.Run();
+
+//this is for tests
+public partial class Program
+{
+}
